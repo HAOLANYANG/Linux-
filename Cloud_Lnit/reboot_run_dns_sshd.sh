7dfaa18286cf57f4bd8dcc2b11b4b9f80bc34b07
HAOLANYANG <1369799123@qq.com>
2022-10-16 01:52:29 +0800

    Update reboot_run_dns_sshd.sh

--- Cloud_Lnit/reboot_run_dns_sshd.sh
+++ Cloud_Lnit/reboot_run_dns_sshd.sh
@@ -1,1 +1,5 @@
-
+#!/bin/bash
+chmod +x /root/change_sshd.sh
+chmod +x /root/change_dns_resolv.conf.sh
+echo '/root/change_sshd.sh' > /etc/rc.d/rc.local
+echo '/root/change_dns_resolv.conf.sh' > /etc/rc.d/rc.local
